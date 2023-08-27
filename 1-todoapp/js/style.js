5460bd9aefab6bae9f4c2511f4c2db230eb49cb0
DmGawron <d.g24@interia.pl>
2023-08-27 14:34:13 +0200

    changed folder

--- 1-todoapp/js/style.js
+++ 1-todoapp/js/style.js
@@ -1,2 +1,18 @@
 
+const removeTask = (e) => {
+    // e.target.parentElement.remove()
+    e.target.parentElement.style.textDecoration = 'line-through'
+   
+}
 
+
+
+
+
+
+
+
+
+document.querySelectorAll('li button').forEach(item => {
+    item.addEventListener('click', removeTask)
+})
--- dokopiowania/js/style.js
+++ dokopiowania/js/style.js
@@ -1,18 +1,1 @@
 
-const removeTask = (e) => {
-    // e.target.parentElement.remove()
-    e.target.parentElement.style.textDecoration = 'line-through'
-   
-}
-
-
-
-
-
-
-
-
-
-document.querySelectorAll('li button').forEach(item => {
-    item.addEventListener('click', removeTask)
-})
