f3f733583ab152ea5f93f9e4ccf05948dd6db76e
DmGawron <d.g24@interia.pl>
2023-08-27 17:09:48 +0200

    update 4

--- 2-todoapp2/js/style.js
+++ 2-todoapp2/js/style.js
@@ -2,10 +2,32 @@
 const form = document.querySelector('form');
 const taskNumber = document.querySelector('h1 span');
 const ul = document.querySelector('ul');
-const listItems = document.querySelectorAll('li')
+const input = document.querySelector('input')
+const listItems = document.getElementsByClassName('task')
+
+const removeTask = (e) => {
+    e.target.parentNode.remove()
+    taskNumber.textContent = listItems.length
+}
 
 
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value
+    console.log(titleTask);
+    if(titleTask === '') return
+    const task = document.createElement('li')
+    task.className = 'task'
+    task.innerHTML = titleTask + '<button>Usuń</button>'
+    ul.appendChild(task)
+    input.value = ''
+    // const listItems = document.querySelectorAll('li.task').length
+    taskNumber.textContent = listItems.length
+    task.querySelector('button').addEventListener('click', removeTask)
+}
 
+
+form.addEventListener('submit', addTask)
 
 
 
