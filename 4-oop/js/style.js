1ba26e734123457440d59a2b0edebb336349a66f
DmGawron <d.g24@interia.pl>
2023-08-29 12:54:27 +0200

    update

--- 4-oop/js/style.js
+++ 4-oop/js/style.js
@@ -8,4 +8,51 @@ const polonez = new Car('polonez');
 
 maluch.name = 'zabytek'
 
+////////////////prototyp//////////////
+
+const Person  = function(name,age) {
+    this.name = name;
+    this.age = age;
+    this.children = []
+}
+
+Person.prototype.addChildren = function(name) {
+this.children.push(name)
+}
+
+const arek = new Person('arek', 20)
+const jacek = new Person('jacek', 23)
+arek.addChildren('wacek')
+
+
+function Player () {
+
+}
+
+const janek = new Player()
+
+class User {
+
+}
+
+const marta = new User()
+const darek = new janek.constructor()
+
+function Citizen(country ,citizenship) {
+    this.country = country
+    this.citizenship = citizenship
+    // this.changeCitizenship = function(citizenship) {
+    //     this.citizenship = citizenship
+    //     console.log(`zmiana za pomocą metow w instancji na obywatelstwo ${this.citizenship}`)}
+    }
+
+
+Citizen.prototype.changeCitizenship = function(citizenship) {
+    this.citizenship = citizenship
+    console.log(`zmiana za pomocą metow w prototypie na obywatelstwo ${this.citizenship}`)
+}
+
+const zenek = new Citizen('Polska', 'polskie')
+
+Citizen.prototype.age = 21
 
