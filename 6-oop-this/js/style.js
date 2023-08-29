c8d8bed3872a6d542510658bba11d545046d987c
DmGawron <d.g24@interia.pl>
2023-08-29 18:10:06 +0200

    update bub

--- 6-oop-this/js/style.js
+++ 6-oop-this/js/style.js
@@ -13,4 +13,37 @@ const obj = {
 obj.showName()
 
 
+document.addEventListener('click', function() {
+    console.log(this);
+    const inside = function () {
+        console.log(this);
+    }
+    inside()
+})
 
+function showName() {
+   console.log(this.name)
+}
+
+const karol = {
+    name: 'karol',
+    showName: showName
+}
+karol.showName()
+
+
+
+function one() {
+    this.name = 'pierwsza'
+    return this.name
+}
+
+const object1 = {
+    two: one,
+    four: function() {
+        console.log(this);
+    }
+}
+
+object1.two()
+object1.four()
